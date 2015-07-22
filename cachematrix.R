5782b7f3d3d964b2dccfc600294c56efedb7ea50
alireza <alirezaabdoli@outlook.com>
2015-07-22 22:55:25 +0430

    Second Commit of Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,49 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two function aimed at creating a matrix
+## and consequently getting the inverse of the matrix in case
+## already cached otherwise the inverse of matrix is calculated.
 
-## Write a short comment describing this function
+
+## The first function 'makeCacheMatrix' creates a matrix and
+## provides a set of functions for manipulating the matrix
 
 makeCacheMatrix <- function(x = matrix()) 
 {
-      m = NULL
-      set = function(y)
+      inv = NULL
+      ## Setting the initial matrix
+      set_matrix = function(y)
       {
             x <<- y
-            m <<- NULL
+            inv <<- NULL
       }
-      
-      get = function()
+      ## Getting the initial matrix
+      get_matrix = function()
       {
             x
       }
-      setmean = function(mean)
+      ## Setting the inversed matrix
+      set_inverse = function(inverse)
       {
-            m <<- mean
+            inv <<- inverse
+      }
+      ## Getting the inversed matrix
+      get_inverse = function()
+      {
+            inv
       }
       
-      getmean = function()
-      {
-            m
-      }
-      
-      list(set = set, get = get, setmean = setmean, getmean = getmean)
-
+      list(set_matrix = set_matrix, get_matrix = get_matrix, 
+           set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## The second function 'cacheSolve' returns the inverse of the matrix
+## if already cached; otherwise the function calculates inverse of
+## the matrix.
 
 cacheSolve <- function(x, ...) 
 {
-        ## Return a matrix that is the inverse of 'x'
-      m = x$getmean()
-      if (!is.null(m))
+      ## Return a matrix that is the inverse of 'x'
+      inv = x$get_inverse()
+      if (!is.null(inv))
       {
-            message("Getting cached matrix")
-            return(m)
+            message("Getting the cached matrix")
+            return(inv)
       }
-            cachedmatrix = x$get()
-            m = solve(cachedmatrix, ...)
-            x$setmean(m)
-            m
+      ## Otherwise inverse of the matrix is calculated and cached
+            matrix = x$get_matrix()
+            inv = solve(matrix, ...)
+            x$set_inverse(inv)
+            inv
 }
